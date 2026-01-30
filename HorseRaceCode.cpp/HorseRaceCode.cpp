7d05f7b539bc0d3e6b82c151a850d8ad451b17b0
janblocker2003 <janblocker2003@gmail.com>
2026-01-30 17:31:10 +0000

    syntax for main corrected

--- HorseRaceCode.cpp/HorseRaceCode.cpp
+++ HorseRaceCode.cpp/HorseRaceCode.cpp
@@ -8,15 +8,20 @@ bool isWinner(int horseNum, int* horses);
 int main(){
 	int horseNum[] = {0, 1, 2, 3, 4};
 	int horses[] = {0, 0, 0, 0, 0};
-	bool keepGoing = True;
-	while keepGoing == True;
-	for (i = 0, i < 5; i++){
-		advance(int i, int* horses[i]);
-		printLane(int i, int* horses[i]);
-		isWinner(int i, int* horses[i]);
-		if (isWinner == True) {
-			keepGoing = False;
+	bool keepGoing = true;
+	while (keepGoing == true){
+	int i = 0;
+	for (i = 0; i < 5; i++){
+		advance(i, &horses[i]);
+		printLane(i, &horses[i]);
+		//bool horseWon = false;
+		bool horseWon = isWinner(i, &horses[i]);
+		if (horseWon == true) {
+			keepGoing = false;
 		} //end if
 		else {
-			keepGoing = True;
+			keepGoing = true;
 		} //end else
+	} //end for
+	} //end while
+} //end main
